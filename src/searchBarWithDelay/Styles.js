9010e5d47ce396201ae15549d553eda7607c6b19
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-08-04 20:07:38 +1200

    Removed search bar with delay margin

--- src/searchBarWithDelay/Styles.js
+++ src/searchBarWithDelay/Styles.js
@@ -5,9 +5,6 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     flex: 1,
-    // padding: 5,
-    marginLeft: 10,
-    marginRight: 10,
   },
   search: {
     backgroundColor: 'transparent',
