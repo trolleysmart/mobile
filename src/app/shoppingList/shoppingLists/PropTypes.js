fe65788f5e50e4b49f75fd4e05fdd98390c0dfa9
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-09-23 15:28:13 +1200

    Added items count + ciode refactoring

--- src/app/shoppingList/shoppingLists/PropTypes.js
+++ src/app/shoppingList/shoppingLists/PropTypes.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 export const ShoppingListProp = PropTypes.shape({
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  totalItemsCount: PropTypes.number.isRequired,
 }).isRequired;
 
 export const ShoppingListsProp = PropTypes.arrayOf(ShoppingListProp);
