3813372512543f05786ac2e9e8cb049e8d23eb2b
fred <fred.zhang.fz@gmail.com>
2017-10-07 22:17:07 +1300

    Refactored staple sections to improve performance issue
    Render each section with flat list

--- src/app/stapleItems/Styles.js
+++ src/app/stapleItems/Styles.js
@@ -1,7 +1,7 @@
 // @flow
 
 import { StyleSheet, Platform } from 'react-native';
-import { Sizes } from '../../framework/style/DefaultStyles';
+import { Sizes, Color } from '../../framework/style/DefaultStyles';
 
 export default StyleSheet.create({
   container: {
@@ -26,6 +26,12 @@ export default StyleSheet.create({
     fontSize: 10,
     textAlign: 'center',
   },
+  itemNameSelected: {
+    fontSize: 10,
+    fontWeight: '500',
+    color: '#3a69e0',
+    textAlign: 'center',
+  },
   sectionHeaderImage: {
     width: 25,
     height: 25,
@@ -43,7 +49,9 @@ export default StyleSheet.create({
     justifyContent: 'center',
   },
   itemsCount: {
-    color: 'blue',
+    fontSize: 14,
+    fontWeight: '600',
+    color: '#3a69e0',
   },
   select: {
     color: 'blue',
@@ -53,8 +61,8 @@ export default StyleSheet.create({
   },
   sectionListContainer: {
     // flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
+    // flexDirection: 'row',
+    // flexWrap: 'wrap',
     // justifyContent: 'center',
   },
   addItemsHeaderContainer: {
